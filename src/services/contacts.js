dd960ce749862c9d9bdcb83d4d9d195418a05860
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-11-17 19:02:25 +0200

    update

---
+++ src/services/contacts.js
@@ -0,0 +1,16 @@
+// import { Contacts } from '../services/contacts.js';
+import { ContactsCollection } from '../db/models/contacts.js';
+
+export const getAllContacts = async () => {
+  const contacts = await ContactsCollection.find();
+  return contacts;
+};
+
+export const getContactById = async (id) => {
+  try {
+    const contact = await ContactsCollection.findById(id);
+    return contact;
+  } catch (error) {
+    return null;
+  }
+};
