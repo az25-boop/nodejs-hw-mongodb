791dee0cd9eb668c10d9348600ce524c6d0927d5
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-11-24 12:02:00 +0200

    midl

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,9 +4,20 @@ import Joi from 'joi';
 
 export const createContactsSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
+  phoneNumber: Joi.number().required(),
   email: Joi.string().email().required(),
-  age: Joi.number().integer().min(6).max(16).required(),
-  gender: Joi.string().valid('male', 'female', 'other').required(),
-  avgMark: Joi.number().min(2).max(12).required(),
-  onDuty: Joi.boolean(),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string()
+    .min(3)
+    .max(30)
+    .valid('work', 'home', 'personal')
+    .required(),
+});
+
+export const updateContactsSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  phoneNumber: Joi.number(),
+  email: Joi.string().email(),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(30).valid('work', 'home', 'personal'),
 });
