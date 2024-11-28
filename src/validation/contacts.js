5232480ff1ebef1c8c7bfbf46d3dae2f3ed664bb
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-11-28 20:23:54 +0200

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,7 +6,7 @@ export const createContactsSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.number().required(),
   email: Joi.string().email().required(),
-  isFavorite: Joi.boolean(),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string()
     .min(3)
     .max(20)
@@ -18,6 +18,6 @@ export const updateContactsSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.number(),
   email: Joi.string().email(),
-  isFavorite: Joi.boolean(),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
 });
