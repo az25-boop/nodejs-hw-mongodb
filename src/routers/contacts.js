9c1ed2ca8f9919e9fe14c54f65bdcab6fbe90fdb
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-12-01 21:03:41 +0200

    middl

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,14 +8,13 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-
 import { isValidId } from '../middlewares/isValidId.js';
 import { validateBody } from '../middlewares/validateBody.js';
-
 import {
   createContactsSchema,
   updateContactsSchema,
 } from '../validation/contacts.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
@@ -47,5 +46,9 @@ router.delete(
   isValidId,
   ctrlWrapper(deleteContactController),
 );
+
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getContactsController));
 
 export default router;
