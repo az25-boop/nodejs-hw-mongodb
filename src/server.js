66dd0d01a5664fc208dd7dc0c919daf64c3624a2
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-12-20 18:33:52 +0200

    update

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import errorHandler from './middlewares/errorHandler.js';
 // import logger from './middlewares/logger.js';
 import authRouter from './routers/auth.js';
 import cookieParser from 'cookie-parser';
-import { swaggerDocs } from './middlewares/swaggerDocs.js';
+// import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 // console.log(process.env.PORT); // тут будуть налаштування компа або сервера на якому запускаємо, тому можуть бути різні налащтування PORT
 
@@ -18,7 +18,7 @@ export const startServer = () => {
   app.use(cors()); // корототкий запис створення і використання мідлвару CORS
   app.use(express.json());
   app.use(cookieParser());
-  app.use('/api-docs', swaggerDocs);
+  // app.use('/api-docs', swaggerDocs);
   app.use(express.static('uploads'));
 
   // routes;
