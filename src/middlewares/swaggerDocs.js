924b3a7ed1bb16b1f6155857683d46d2ecac5b44
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-12-22 22:51:11 +0200

    Update swaggerDocs.js

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -3,13 +3,13 @@ import swaggerUI from 'swagger-ui-express';
 import fs from 'node:fs';
 import { SWAGGER_PATH } from '../constants/index.js';
 
-export const swaggerDocuments = (req, res, next) => {
-	try {
-		const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-		swaggerUI.setup(swaggerDoc)(req, res, next);
-	} catch {
-		next(createHttpError(500, "Can't load swagger docs"));
-	}
+export const swaggerDocs = () => {
+  try {
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+  } catch (error) {
+    return (req, res, next) => {
+      next(createHttpError(500, "Can't load swagger docs"));
+    };
+  }
 };
-
-export const swaggerDocs = [swaggerUI.serve, swaggerDocuments];
