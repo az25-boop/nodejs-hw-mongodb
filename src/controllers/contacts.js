4b439caa24babef6510dc9a61c4d835e24178b06
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-12-22 18:29:12 +0200

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -116,7 +116,7 @@ export const patchContactController = async (req, res) => {
   res.json({
     status: 200,
     message: 'Successfully patched a contact!',
-    data: result.contact,
+    data: result.updatedContact,
   });
 };
 
